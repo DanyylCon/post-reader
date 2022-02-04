ab67364099417a767152c1051526e01860885ecd
Danyyl <danyyl.con@gmail.com>
2022-02-04 18:03:07 +0200

    introduce bootstrap library and use httpclient and observables for data

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Post } from '../models/Post';
+import { PostService } from '../services/post.service';
 
 @Component({
   selector: 'app-posts',
@@ -11,29 +12,16 @@ export class PostsComponent implements OnInit {
   title: string = 'Posts';
   posts: Post[] = [];
 
-  constructor() { }
+  constructor(private postService: PostService) { }
 
   ngOnInit(): void {
-    this.posts = [
-      {
-        id: 1,
-        title: "My first post",
-        body: "Just testing this out!",
-        votes: 1
-      },
-      {
-        id: 2,
-        title: "What's your favorite front-end framework?",
-        body: "My favorite is Angular. What do you enjoy working with?",
-        votes: 1
-      },
-      {
-        id: 3,
-        title: "ngOnInit is great",
-        body: "This lifecycle method gets called automatically upon component initialization!",
-        votes: 1
+    this.postService.getPosts().subscribe(res => {
+      for(let index = 0; index < res.length; index++){
+        const post = res[index];
+        post['votes'] = 1;
       }
-    ];
+      this.posts = res;
+    });
   }
 
   hidePost(post: Post): void{
